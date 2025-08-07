ee48cdee6a9f746d25569e8cce12392a1bb9d5a5
TheOrlangur <theorlangur@gmail.com>
2025-08-07 22:31:06 +0200

    changed ring buffer implementation to use power of 2 automatically

--- include/lib_ring_buffer.hpp
+++ include/lib_ring_buffer.hpp
@@ -6,58 +6,70 @@
 #include "lib_misc_helpers.hpp"
 #include "lib_type_traits.hpp"
 
+template<class T> requires std::is_integral_v<T>
+constexpr int HighestBitSet(T N)
+{
+    int r = -1;
+    while(N) 
+    {
+        N >>= 1;
+        ++r;
+    }
+    return r;
+}
+
 template<class T, size_t N>
 struct RingBuffer
 {
     using size_type = MinSizeType<N>::type;
+    constexpr static int kHighestBit = HighestBitSet(size_type(N));
+    constexpr static size_type kBufSize = kHighestBit >= 0 ? size_type(1) << (kHighestBit + 1) : 0;
+    constexpr static size_type kMask = kBufSize - 1;
 
-    template<class... Args>
+    template<class... Args> requires (N > 0)
     std::optional<size_type> push(Args&&...args)
     {
-        if (m_Full) return std::nullopt;
-        new (&(m_Buf[m_Tail++].item)) T{std::forward<Args>(args)...};
-        m_Tail %= N;
-        m_Full = m_Tail == m_Head;
-        return size_type(((N + m_Tail) - m_Head) % N);
+        auto newTail = (m_Tail + 1) & kMask;
+        if (newTail == m_Head) return std::nullopt;
+        m_Tail = newTail;
+        new (&(m_Buf[m_Tail].item)) T{std::forward<Args>(args)...};
+        return size_type(((kBufSize + m_Tail) - m_Head) & kMask);
     }
 
-    std::optional<T> pop()
+    std::optional<T> pop() requires (N > 0)
     {
-        if (m_Tail == m_Head && !m_Full) return std::nullopt;
+        if (m_Tail == m_Head) return std::nullopt;
         ScopeExit cleanup = [&]{
             if constexpr (!std::is_trivially_destructible_v<T>)
                 m_Buf[m_Head].item.~T();
-            m_Head = (m_Head + 1) % N;
-            m_Full = false;
         };
+        m_Head = (m_Head + 1) & kMask;
         return m_Buf[m_Head].item;
     }
 
-    bool drop()
+    bool drop() requires (N > 0)
     {
-        if (m_Tail == m_Head && !m_Full) return false;
+        if (m_Tail == m_Head) return false;
 
+        m_Head = (m_Head + 1) & kMask;
         if constexpr (!std::is_trivially_destructible_v<T>)
             m_Buf[m_Head].item.~T();
-        m_Head = (m_Head + 1) % N;
-        m_Full = false;
         return true;
     }
 
-    T* peek()
+    T* peek() requires (N > 0)
     {
-        if (m_Tail == m_Head && !m_Full) return nullptr;
-        return &m_Buf[m_Head].item;
+        if (m_Tail == m_Head) return nullptr;
+        return &m_Buf[(m_Head + 1) & kMask].item;
     }
 private:
     union Raw
     {
         T item;
     };
-    Raw m_Buf[N];
+    [[no_unique_address]]Raw m_Buf[kBufSize];
     size_type m_Head = 0;
     size_type m_Tail = 0;
-    bool m_Full = false;
 };
 
 #endif
